49f5dbd9122b3b5f195023ad3c980b59aa8c1bc6
Elpis <umuttuzlu0@protonmail.com>
2025-06-01 14:30:21 +0300

    Languages options added

--- UI/place.py
+++ UI/place.py
@@ -5,7 +5,10 @@ def test(app):
     text = components.TextBox(app.scene, 25,25, 100, 100,("Arial",12), "normal")
 
 def user_text(app):
-    user = components.TextBox(app.scene, 25, 25, 502.5, 450, ("Arial", 12), "normal")
+    user = components.TextBox(app.scene, 25, 25, 502.5, 600, ("Arial", 12), "normal")
 def translate_text(app):
-    user = components.TextBox(app.scene, 540, 25, 502.5, 450, ("Arial", 12), "normal")
+    user = components.TextBox(app.scene, 540, 25, 502.5, 600, ("Arial", 12), "normal")
     user.disable()
+
+def lang(app):
+    langs = components.combo(app.scene, 25, 650, "Languages", ("Binary", "Mors"), "readonly")
