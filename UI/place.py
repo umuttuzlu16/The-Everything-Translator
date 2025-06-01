ce6d80dfc5348dc618c57367aa597b0624adad1c
Elpis <umuttuzlu0@protonmail.com>
2025-06-01 14:20:24 +0300

    user textbox added

--- UI/place.py
+++ UI/place.py
@@ -1,5 +1,8 @@
-import components
+from UI import components
 
 
 def test(app):
     text = components.TextBox(app.scene, 25,25, 100, 100,("Arial",12), "normal")
+
+def user_text(app):
+    user = components.TextBox(app.scene, 25, 25, 515, 60, ("Arial", 12), "normal")
