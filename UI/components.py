c5456d9356dd463cf35562f8e448cd3f276ee2ba
Elpis <umuttuzlu0@protonmail.com>
2025-06-01 12:20:41 +0300

    Button better now

--- UI/components.py
+++ UI/components.py
@@ -3,8 +3,21 @@ from tkinter import ttk
 
 
 class btn():
-    def __init__(self, frame, x, y, width, height, text, color, command):
-        self.button = tk.Button(frame, text=text, bg=color, command=command)
-        self.button.place(x=x, y=y, height=height, width=width)
-        
+    def __init__(self, frame, x, y, width, height, text, bg, command, fg, activebackground, activeforeground, font, cursor):
+        self.button = tk.Button(
+            frame,
+            text=text,
+            bg=bg,
+            fg=fg,
+            activebackground=activebackground,
+            activeforeground=activeforeground,
+            font=font,   # font=24 hatalıydı, tuple formatında olmalı
+            height=height,
+            width=width,
+            cursor=cursor,
+            command=command
+        )
+        self.button.place(x=x, y=y)
+
+
         
