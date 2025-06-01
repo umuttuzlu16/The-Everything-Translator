765881da41471a9a5730b781a750401efe27f553
Elpis <umuttuzlu0@protonmail.com>
2025-06-01 14:25:46 +0300

    Trasnlated text stage added

--- UI/place.py
+++ UI/place.py
@@ -5,4 +5,7 @@ def test(app):
     text = components.TextBox(app.scene, 25,25, 100, 100,("Arial",12), "normal")
 
 def user_text(app):
-    user = components.TextBox(app.scene, 25, 25, 515, 60, ("Arial", 12), "normal")
+    user = components.TextBox(app.scene, 25, 25, 502.5, 450, ("Arial", 12), "normal")
+def translate_text(app):
+    user = components.TextBox(app.scene, 540, 25, 502.5, 450, ("Arial", 12), "normal")
+    user.disable()
