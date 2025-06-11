e9679c82375493b0d0878e10bb8d36b50fc75029
Elpis <umuttuzlu0@protonmail.com>
2025-06-11 23:01:46 +0300

    Error Messages Added

--- Translates/binary.py
+++ Translates/binary.py
@@ -3,18 +3,24 @@
 from UI import place
 
 def binary(app):
-    text = text = place.user.get()
-    translated = ' '.join(format(ord(char), '08b') for char in text)
+    try:
+        text = text = place.user.get()
+        translated = ' '.join(format(ord(char), '08b') for char in text)
 
-    place.trans.enable()
-    place.trans.set(translated)
-    place.trans.disable()
+        place.trans.enable()
+        place.trans.set(translated)
+        place.trans.disable()
+    except:
+        place.trans.set("Invalid Text")
 
 def ascii(app):
-    text = place.user.get()
-    text = text.replace(" ", "")
-    translated = ''.join(chr(int(text[i:i+8], 2)) for i in range(0, len(text), 8))
+    try:
+        text = place.user.get()
+        text = text.replace(" ", "")
+        translated = ''.join(chr(int(text[i:i+8], 2)) for i in range(0, len(text), 8))
 
-    place.trans.enable()
-    place.trans.set(translated)
-    place.trans.disable()
+        place.trans.enable()
+        place.trans.set(translated)
+        place.trans.disable()
+    except:
+        place.trans.set("Invalid Text")
